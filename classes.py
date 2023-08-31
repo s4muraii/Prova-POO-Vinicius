8e331cb587a9aa4eaac50f48bd9e2e50f6cf6eab
s4muraii <viniciusoliveiramoura24@gmail.com>
2023-08-31 09:00:01 -0300

    criação def e set

--- classes.py
+++ classes.py
@@ -2,14 +2,29 @@ tarefas = {}
 
 class ToDoList:
     def adicionar_tarefa(self, descricao: str):
-        self.descricao = descricao
+        self.__descricao = descricao
         return descricao
 
     def excluir_tarefa(self, indice: int):
-        self.indice = indice
+        self.__indice = indice
         for indice in tarefas:
             -indice
 
     def listar_tarefas(self):
         print(tarefas)
         return tarefas
+    
+    def get_Descricao(self, descricao):
+        return descricao
+    
+    def set_Descricao(self, nova_descricao):
+        self.__descricao = nova_descricao
+        return nova_descricao
+    
+    def get_Descricao(self, indice):
+        return indice
+    
+    def set_Descricao(self, novo_indice):
+        self.__indice = novo_indice
+        return novo_indice
+    
